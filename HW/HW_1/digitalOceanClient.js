5c81eb525c606ef06468658ef7fe2941577b2574
Nirmesh <nirmesh.khandelwal@gmail.com>
2015-09-13 04:12:12 -0400

    initializing digital ocean client

---
+++ HW/HW_1/digitalOceanClient.js
@@ -0,0 +1,16 @@
+exports.createVM = function(subscription_id){
+    //A function to spawn a new vm on digital ocean platform
+    console.log('hello world');
+};
+
+exports.destroyVM = function(){
+    //A function to destroy a vm on digital ocean platform
+};
+
+exports.installAnsible = function(){
+    //A function to install ansible on VM
+};
+
+exports.createInventoryFile = function(){
+    // A function to automatically create inventory file on VM
+};
