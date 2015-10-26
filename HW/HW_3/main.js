2dc2e6e66e920ae618e58d1f502e3ad6bbc28563
Nirmesh <nirmesh.khandelwal@gmail.com>
2015-10-25 20:26:08 -0400

    Successfully completed the requirements

--- HW/HW_3/main.js
+++ HW/HW_3/main.js
@@ -1,21 +1,20 @@
 var redis = require('redis')
-var multer  = require('multer')
+var multer = require('multer')
 var express = require('express')
-var fs      = require('fs')
+var fs = require('fs')
 var app = express()
-var upload = multer({ dest: './uploads/' })
-// REDIS
+var upload = multer({
+    dest: './uploads/'
+  })
+  // REDIS
+var args = process.argv.slice(2);
+var port = args[0] || 3000
 var client = redis.createClient(6379, '127.0.0.1', {})
 
-///////////// WEB ROUTES
-
-
-// Your own super cool function
-
-// Add hook to make it easier to get all visited URLS.
-app.use(function(req, res, next)
-{
-  console.log(req.method, req.url);
+app.use(function(req, res, next) {
+	//console.log(req)
+  console.log(req.url);
+  console.log(req.get('host'));
   client.lpush('recentUrl', req.protocol + '://' + req.get('host') + req.url)
   client.ltrim('recentUrl', 0, 4)
   next();
@@ -24,81 +23,112 @@ app.use(function(req, res, next)
 //app.use(app.router); // The Express routes handler.
 
 app.get('/get', function(req, res) {
-   {
-      client.get("myKey", function(err, value){
-      res.writeHead(200, {'content-type':'text/html'});
-      if(value) res.write(value);
+  {
+    client.get("myKey", function(err, value) {
+      res.writeHead(200, {
+        'content-type': 'text/html'
+      });
+      if (value) res.write(value);
       else res.write('<p> Key not found </p>');
       console.log(value)
       res.end();
     });
-   }
+  }
 })
 
 app.get('/set', function(req, res) {
-   {
+  {
     client.set('myKey', 'this message will self-destruct in 10 seconds')
     client.expire('myKey', 10)
-     res.writeHead(200, {'content-type':'text/html'});
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    });
     res.write("<p> myKey set to expire for 10 seconds </p>");
     res.end();
-   }
+  }
 })
 
 app.get('/recent', function(req, res) {
-   {
-    res.writeHead(200, {'content-type':'text/html'});
+  {
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    });
     var responseString = "<ul>"
-    client.lrange('recentUrl', 0, -1, function(err, value){
+    client.lrange('recentUrl', 0, -1, function(err, value) {
       console.log(value)
-      for (var i=0; i < value.length; i++){
+      for (var i = 0; i < value.length; i++) {
         responseString += "<li>" + value[i] + "</li>"
       }
       responseString += "</ul>"
       res.write(responseString);
       res.end();
     })
-   }
+  }
 })
 
-app.post('/upload', upload.array('image') , function(req, res){
-	//console.log(req)
-	console.log(req.body) // form fields
+app.post('/upload', upload.array('image'), function(req, res) {
+  //console.log(req)
+  console.log(req.body) // form fields
   console.log(req.files) // form files
 
-	for(var i=0; i < req.files.length; i++){
-		console.log(req.files[i])
-		if( req.files[i] )
-	  {
-	     fs.readFile( req.files[i].path, function (err, data) {
-	        if (err) throw err;
-	        var img = new Buffer(data).toString('base64');
-					client.rpush('images', img)
-	        console.log(img);
-	    });
-	  }
-	}
+  for (var i = 0; i < req.files.length; i++) {
+    console.log(req.files[i])
+    if (req.files[i]) {
+      fs.readFile(req.files[i].path, function(err, data) {
+        if (err) throw err;
+        var img = new Buffer(data).toString('base64');
+        client.rpush('images', img)
+        console.log(img);
+      });
+    }
+  }
   res.status(204).end()
 });
 
 app.get('/meow', function(req, res) {
   {
-    res.writeHead(200, {'content-type':'text/html'});
-		client.rpop('images', function(err, val){
-			if (err) throw err;
-			if(val){
-				res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+val+"'/>");
-			}else{
-				res.write("No new image to display")
-			}
-			res.end();
-		})
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    });
+    client.rpop('images', function(err, val) {
+      if (err) throw err;
+      if (val) {
+        res.write("<h1>\n<img src='data:my_pic.jpg;base64," + val + "'/>");
+      } else {
+        res.write("No new image to display")
+      }
+      res.end();
+    })
   }
 })
 
-// HTTP SERVER
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log('Example app listening at http://%s:%s', host, port)
- })
+
+// HTTP SERVER 1
+var server1 = app.listen(port, function(err, val) {
+  var host = this.address().address
+  var port = this.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+})
+
+/*
+// HTTP SERVER 2
+var server2 = app.listen(3001, function() {
+  var host = this.address().address
+  var port = this.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+})
+
+// HTTP SERVER 3
+var server3 = app.listen(3002, function() {
+  var host = this.address().address
+  var port = this.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+})
+
+// HTTP SERVER 3
+var server3 = app.listen(3003, function() {
+  var host = this.address().address
+  var port = this.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+})
+*/
---
+++ HW/HW_3/proxy.js
@@ -0,0 +1,44 @@
+var redis = require('redis')
+var client = redis.createClient(6379, '127.0.0.1', {})
+var http = require("http");
+var proxy = require('http-proxy');
+
+var servers = [ 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002' ]
+
+// Push the server indexes into redis.
+client.del('server_index')
+client.rpush('server_index', 0)
+client.rpush('server_index', 1)
+client.rpush('server_index', 2)
+
+var next = function(callback) {
+
+}
+
+// Create a proxy object for each target.
+var proxies = servers.map(function(target) {
+  return new proxy.createProxyServer({
+    target: target,
+    ws: true,
+    xfwd: true
+  });
+});
+
+var proxy = function(req, res) {
+  console.log("request received")
+  //Use circular list of redis using RPOPLPUSH
+  client.rpoplpush('server_index', 'server_index', function(err, index) {
+    if (err) throw err
+    console.log(index)
+    var proxy = proxies[index];
+    proxy.web(req, res);
+  })
+}
+
+var server = http.createServer(proxy);
+
+server.listen(80, function() {
+  var host = this.address().address
+  var port = this.address().port
+  console.log('Proxy Server listening at http://%s:%s', host, port)
+});
